ef9caf221758dc0f485bd113e90720fd4bbf6970
Hongjun <ggaye159@naver.com>
2022-10-14 02:52:39 +0900

    Update mysql.js

--- server/config/mysql.js
+++ server/config/mysql.js
@@ -2,10 +2,10 @@ import mysql from'mysql2/promise';
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: 'circle.cfq3dtw0s8pz.ap-northeast-2.rds.amazonaws.com',
+    host: process.env.Sqlhost,
     user: 'admin',
     port: '3306',
-    password: 'ghdwns12',
+    password: process.env.SqlPW,
     database: 'Circle'
 });
 
