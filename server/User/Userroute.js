dfc0a179a685091fd01fae8bc1b0a74c92f49fbe
Hongjun <ggaye159@naver.com>
2022-10-14 16:42:14 +0900

    cors set

--- server/User/Userroute.js
+++ server/User/Userroute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import Usercontrol from './Usercontrol.js';
+import cors from'cors';
 
 class Router{
     constructor(){
+        this.app=express();
         this.router=express.Router();
         this.setRouter();
     }
@@ -10,6 +12,10 @@ class Router{
         this.router.post('/Login',Usercontrol.process.login);
         this.router.post('/Signin',Usercontrol.process.Signin);
         this.router.get('/Mypage',Usercontrol.process.Mypage);
+        this.app.use(cors({
+            origin: '*', 
+            credential: 'true' 
+        }));
     }
 }
 
